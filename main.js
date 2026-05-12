924001721cd7b5801dfd51289b3a5ac1b9378b5c
Muhammadumerakmal <umeraura99@gmail.com>
2026-05-12 20:10:27 +0500

    moving towards end

---
+++ main.js
@@ -0,0 +1,15 @@
+// main.js
+
+import dotenv from "dotenv"
+dotenv.config()
+import express from "express"
+import { loadavg } from "node:os"
+
+const app = express()
+
+const secret_port = process.env.PORT || 5000
+
+const server = app.listen(secret_port,()=>{
+    console.log("server is running on secret port 🔐");
+    
+})
