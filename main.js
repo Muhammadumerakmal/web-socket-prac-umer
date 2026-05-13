af52ef19732abfc8cf711a0ca88f4ae3fee27d76
Muhammadumerakmal <umeraura99@gmail.com>
2026-05-13 16:21:49 +0500

    completed

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 import dotenv from "dotenv";
 dotenv.config();
+
 import express from "express";
 import { WebSocketServer } from "ws";
 
@@ -12,20 +13,27 @@ const app = express();
 const secret_port = process.env.PORT || 5000;
 
 const server = app.listen(secret_port, () => {
-  console.log("server is running on secret port 🔐");
+  console.log(`server is running on secret port 🔐 ${secret_port}`);
 });
-
 
 const wss = new WebSocketServer({ server });
 
+wss.on("connection", (webs, req) => {
+  // Get client IP address
+  const ip =
+    req.headers["x-forwarded-for"] || // for proxy/ngrok/vercel
+    req.socket.remoteAddress;
 
-wss.on("connection",(webs)=>{
-    console.log(" 🔌 websocket connected on secret port 🌐");
+  console.log("🔌 websocket connected on secret port 🌐");
+  console.log("🌍 User IP Address:", ip);
 
-    webs.on("message",(data)=>{
-        console.log("✅ %s" , data);
-        
-        webs.send("thanks for your message 😊")
-        
-    })
-})
+  webs.on("message", (data) => {
+    console.log(`✅ Message from ${ip}: ${data}`);
+
+    webs.send("thanks for your message 😊");
+  });
+
+  webs.on("close", () => {
+    console.log(`❌ websocket connection closed for ${ip}`);
+  });
+});
